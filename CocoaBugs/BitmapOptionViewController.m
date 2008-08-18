880fce96e2fcee0fed2e2ced99607e2be85cfc0f
Devin Chalmers <devin.chalmers@gmail.com>
2008-08-18 01:10:54 -0700

    GUI tweak, remove logging

--- CocoaBugs/BitmapOptionViewController.m
+++ CocoaBugs/BitmapOptionViewController.m
@@ -29,7 +29,6 @@
 	NSString *thePath = [[NSBundle bundleForClass:[self class]] pathForResource:@"BuiltInImages" ofType:@"plist"];
 	builtInImages = [[NSMutableArray arrayWithObject:@"Pick an image..."] retain];
 	builtInImages = [[builtInImages arrayByAddingObjectsFromArray:[NSArray arrayWithContentsOfFile:thePath]] retain];
-	NSLog([builtInImages description]);
 	
 	self.name = [options objectForKey:@"name"];
 	self.title = [options objectForKey:@"title"];
@@ -61,22 +60,6 @@
 
 - (NSData *)value;
 {
-//	NSImage *image = [imageView image];
-//	NSBitmapImageRep *bitmap = [NSBitmapImageRep imageRepWithData:[image TIFFRepresentation]];
-//	NSBitmapImageRep *bitmap = [[NSBitmapImageRep alloc] initWithBitmapDataPlanes:NULL pixelsWide:width pixelsHigh:height bitsPerSample:1 samplesPerPixel:1 hasAlpha:0 isPlanar:1 colorSpaceName:NULL bytesPerRow:0 bitsPerPixel:1];
-//	int width = [image size].width;
-//	int height = [image size].height;
-
-//	int i, j;
-//	NSMutableData *data = [NSMutableData dataWithLength:[image size].width * [image size].height];
-//	BOOL sample;
-//	for (i = 0; i < [image size].height; i++) {
-//		for (j = 0; j < [image size].width; j++) {
-//			sample = [[bitmap colorAtX:i y:j] brightnessComponent] < 0.5;
-//			[data appendBytes:&sample length:1];
-//		}
-//	}
-	
 	return [[imageView image] TIFFRepresentationUsingCompression:NSTIFFCompressionLZW factor:0];
 }
 
